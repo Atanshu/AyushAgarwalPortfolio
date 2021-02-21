62ed9b3f2263eb43e9924958d1cef7ff2fb44370
Atanshu Goyal <goelatanshu0106@gmail.com>
2021-02-21 18:38:34 +0530

    Some changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: MyResume - v2.2.1
-* Template URL: https://bootstrapmade.com/free-html-bootstrap-template-my-resume/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 !(function($) {
   "use strict";
 
